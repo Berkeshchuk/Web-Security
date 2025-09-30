87896e2706bf8cabe4370575fa481753111ed8d1
Berkeshchuk <berkeshchuk.oleksandr@chnu.edu.ua>
2025-09-30 22:45:53 +0300

    lab commit

--- web-security-lab2/src/main/java/com/web/security/web_security_lab/Repository/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.web.security.web_security_lab.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.web.security.web_security_lab.Models.HotelRoom;
-
-public interface RoomRepository extends JpaRepository<HotelRoom, Long> {
-    
-}
