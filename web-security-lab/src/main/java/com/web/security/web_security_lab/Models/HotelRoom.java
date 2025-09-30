f1488d3a4c8f8053ac80e3f7affe2cd7ef027d90
Berkeshchuk <berkeshchuk.oleksandr@chnu.edu.ua>
2025-09-30 21:07:27 +0300

    First commit

---
+++ web-security-lab/src/main/java/com/web/security/web_security_lab/Models/HotelRoom.java
@@ -0,0 +1,27 @@
+package com.web.security.web_security_lab.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import com.web.security.web_security_lab.Models.abstraction.ServiceItem;
+import java.util.List;
+
+import lombok.experimental.SuperBuilder;
+import lombok.Data;
+
+@Entity
+@Table(name = "hotel_rooms")
+@SuperBuilder
+@Data
+public class HotelRoom extends ServiceItem {
+    @Column(nullable = false)
+    private float area;
+    @Column(nullable = false)
+    private int rooms;
+
+    @ElementCollection
+    @Column(name = "amenitie")
+    private List<String> amenities;
+}
